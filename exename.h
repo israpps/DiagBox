850b4e2128edfe841973667eeda1bcde2c1661d0
Matías Israelson <57065102+israpps@users.noreply.github.com>
2021-04-26 09:44:04 -0300

    Update exename.h

--- exename.h
+++ exename.h
@@ -1,14 +1,15 @@
 #ifndef _H_EXENAME_H_
 #define _H_EXENAME_H_
+  #include <string>
 
   std::string EXE_NAME (char* _argv0)
   {
   std::string temporary = _argv0;
-  std::string EXENAME;
-  EXENAME = temporary.substr( temporary.find_last_of('\\')+ 1 );
-
-return EXENAME;
+  return temporary.substr( temporary.find_last_of('\\')+ 1 );
   }
 
-
+  std::string EXE_NAME (std::string _argv0)
+  {
+  return _argv0.substr(_argv0.find_last_of('\\') +1 );
+  }
 #endif // _H_EXENAME_H_
