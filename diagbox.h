94b1c249767ee8c2e201d15674af8081928f9fd9
Matías Israelson <57065102+israpps@users.noreply.github.com>
2021-04-26 09:12:24 -0300

    Update diagbox.h

--- diagbox.h
+++ diagbox.h
@@ -2,6 +2,6 @@
 #define DIAGBOX
 #include <windows.h>
 #define COLOR_INT(args) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),args);
-#define COLOR_HEX(args) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),stoi(#args,0,16));
+#define COLOR_HEX(args) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),stoi(args,0,16));
 
 #endif //DIAGBOX
