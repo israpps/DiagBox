7892a541ed35e8ccb5849a3e7f36e53498d94cb9
israpps <isr_apps_help@hotmail.com>
2021-06-05 11:41:53 -0300

    minor update & 32 bits version

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
+/***************************************************************
+ * Name:      main.cpp
+ * Purpose:   program behaviour
+ * Author:    matias israelson (aka: El_isra) (tatochin-m@hotmail.com)
+ * Copyright: matias israelson (aka: El_isra) (https://github.com/israpps)
+ * License:   GPL-3.0
+ **************************************************************/
 #include "main.h"
 
-
-
-//const char* DD = "dd"; ///output given text allowing to change the color of diferent sections WIP
-
-//int stoi_with_hex(string original);
-//void show_colors();
 
 int main(int argc, char* argv[])
 {
@@ -16,26 +17,22 @@ int main(int argc, char* argv[])
         print_help();
     else
     {
-///-------clone arguments as strings///
-    for (int x=0; x<argc; x++)
-        {
-        argvS[x] = argv[x];
-        }
-///---------------------------------///
+        for (int x=0; x<argc; x++)//clone arguments into std::string
+            {argvS[x] = argv[x];}
 
 
-    if ( (argc <=2) && (strcmp(argv[1],CMD_GD) == 0))
-            cout << "missing argument...\nenter the comand: \"" << exename << " help\" to learn how to use this program\n";
+        if ( (argc <=2) && (strcmp(argv[1],CMD_GD) == 0))
+                cout << "missing argument...\nenter the comand: \"" << exename << " help\" to learn how to use this program\n";
 
-    if ( (argc >= 3) && (strcmp(argv[1],CMD_GD) == 0))
-            COLOR_STRING(argvS[2]);
+        if ( (argc >= 3) && (strcmp(argv[1],CMD_GD) == 0))
+                COLOR_STRING(argvS[2]);
 
 
-    if(strcmp(argv[1],CMD_COLORS)==0)
-            show_colors();
+        if(strcmp(argv[1],CMD_COLORS)==0)
+                show_colors();
 
-    if(strcmp(argv[1],CMD_HELP)==0)
-        print_help();
+        if(strcmp(argv[1],CMD_HELP)==0)
+            print_help();
     }
     return 0;
 }
